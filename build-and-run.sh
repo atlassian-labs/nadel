92e082e9c79fe86b57d30f92290c48f7cf0f250f
andimarek <andimarek@fastmail.fm>
2018-06-27 12:21:03 -0700

    build process

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
-imageID=$(docker build . -q)
-docker run -p 8080:8080 -v $(pwd)/nadel-service/nadel-dsl.txt:/nadel-dsl.txt ${imageID}
-
+##!/usr/bin/env bash
+#imageID=$(docker build . -q)
+#docker run -p 8080:8080 -v $(pwd)/nadel-service/nadel-dsl.txt:/nadel-dsl.txt ${imageID}
+#
