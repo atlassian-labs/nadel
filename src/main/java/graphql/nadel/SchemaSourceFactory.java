a7620f863b9e202f831ba7a99109a3f1e6eca239
Tomek Sroka <tsroka@atlassian.com>
2018-08-21 14:54:13 -0700

    Implement support for hydration transformation.

---
+++ src/main/java/graphql/nadel/SchemaSourceFactory.java
@@ -0,0 +1,18 @@
+package graphql.nadel;
+
+
+import com.atlassian.braid.Link;
+import com.atlassian.braid.SchemaNamespace;
+import com.atlassian.braid.SchemaSource;
+import graphql.nadel.dsl.ServiceDefinition;
+import graphql.schema.idl.TypeDefinitionRegistry;
+
+import java.util.List;
+
+/**
+ * Factory for schema source.
+ */
+public interface SchemaSourceFactory {
+    SchemaSource createSchemaSource(ServiceDefinition definition, SchemaNamespace namespace,
+                                    TypeDefinitionRegistry typeDefinitionRegistry, List<Link> links);
+}
