92f50799513dd8461e230facb94afc11bcc5c5be
andimarek <andimarek@fastmail.fm>
2019-01-03 15:04:02 +1100

    error handling of invalid data from the delegated service

--- src/test/groovy/graphql/nadel/engine/DelegatedResultToResultNodeTest.groovy
+++ src/test/groovy/graphql/nadel/engine/DelegatedResultToResultNodeTest.groovy
@@ -4,6 +4,7 @@ package graphql.nadel.engine
 import graphql.execution.nextgen.result.ResultNodesUtil
 import graphql.nadel.DelegatedExecutionResult
 import graphql.nadel.TestUtil
+import spock.lang.Ignore
 import spock.lang.Specification
 
 class DelegatedResultToResultNodeTest extends Specification {
@@ -90,6 +91,66 @@ class DelegatedResultToResultNodeTest extends Specification {
 
         then:
         executionResult.data == data
+    }
+
+
+    def "unknown field"() {
+        def data = ["unknown field": "world"]
+        def schema = TestUtil.schema("type Query{ hello: String }")
+        def query = TestUtil.parseQuery("{hello}")
+
+        def (executionContext, fieldSubSelection) = TestUtil.executionData(schema, query)
+
+        DelegatedResultToResultNode resultToNodes = new DelegatedResultToResultNode()
+        DelegatedExecutionResult delegatedResult = new DelegatedExecutionResult(data)
+
+        when:
+        def node = resultToNodes.resultToResultNode(executionContext, delegatedResult, fieldSubSelection)
+        def executionResult = ResultNodesUtil.toExecutionResult(node)
+
+        then:
+        executionResult.data == ["hello": null]
+    }
+
+    def "invalid return type: List instead Int"() {
+        def data = ["hello": ["world1", "world2"]]
+        def schema = TestUtil.schema("type Query{ hello: Int }")
+        def query = TestUtil.parseQuery("{hello}")
+
+        def (executionContext, fieldSubSelection) = TestUtil.executionData(schema, query)
+
+        DelegatedResultToResultNode resultToNodes = new DelegatedResultToResultNode()
+        DelegatedExecutionResult delegatedResult = new DelegatedExecutionResult(data)
+
+        when:
+        def node = resultToNodes.resultToResultNode(executionContext, delegatedResult, fieldSubSelection)
+        def executionResult = ResultNodesUtil.toExecutionResult(node)
+
+        then:
+        executionResult.data == ["hello": null]
+        executionResult.errors.size() == 1
+    }
+
+    // Fails currently because the String scalar coerces everything to String
+    @Ignore
+    def "invalid return type: List instead String"() {
+        def data = ["hello": ["world1", "world2"]]
+        def schema = TestUtil.schema("type Query{ hello: String }")
+        def query = TestUtil.parseQuery("{hello}")
+
+        def (executionContext, fieldSubSelection) = TestUtil.executionData(schema, query)
+
+        DelegatedResultToResultNode resultToNodes = new DelegatedResultToResultNode()
+        DelegatedExecutionResult delegatedResult = new DelegatedExecutionResult(data)
+
+        when:
+        def node = resultToNodes.resultToResultNode(executionContext, delegatedResult, fieldSubSelection)
+        def executionResult = ResultNodesUtil.toExecutionResult(node)
+
+        then:
+        executionResult.data == ["hello": null]
+        executionResult.errors.size() == 1
+
     }
 
 
