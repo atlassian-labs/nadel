f4f09ce90185eab4aeed74222d772ba72960e874
Brad Baker <bbaker@atlassian.com>
2019-03-25 12:13:01 +1100

    Got some of the tests working

---
+++ src/test/groovy/graphql/nadel/testutils/MockServiceExecution.groovy
@@ -0,0 +1,24 @@
+package graphql.nadel.testutils
+
+import graphql.nadel.ServiceExecution
+import graphql.nadel.ServiceExecutionParameters
+import graphql.nadel.ServiceExecutionResult
+
+import java.util.concurrent.CompletableFuture
+
+class MockServiceExecution implements ServiceExecution {
+    final ServiceExecutionResult result;
+
+    MockServiceExecution(Map<String, Object> data) {
+        this(data, Collections.emptyList())
+    }
+
+    MockServiceExecution(Map<String, Object> data, List<Map<String, Object>> errors) {
+        this.result = new ServiceExecutionResult(data, errors)
+    }
+
+    @Override
+    CompletableFuture<ServiceExecutionResult> execute(ServiceExecutionParameters serviceExecutionParameters) {
+        return CompletableFuture.completedFuture(result)
+    }
+}
