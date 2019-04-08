e301e97cb26083ecc5ac066b657822b4baea0d54
Brad Baker <bbakerman@gmail.com>
2019-04-08 18:39:24 +1000

    Now passes executionId and CacheControl on to Service calls (#53)

--- src/main/java/graphql/nadel/ServiceExecutionParameters.java
+++ src/main/java/graphql/nadel/ServiceExecutionParameters.java
@@ -1,6 +1,8 @@
 package graphql.nadel;
 
 import graphql.PublicApi;
+import graphql.cachecontrol.CacheControl;
+import graphql.execution.ExecutionId;
 import graphql.language.Document;
 import graphql.language.FragmentDefinition;
 import graphql.language.OperationDefinition;
@@ -19,13 +21,17 @@ public class ServiceExecutionParameters {
     private final Map<String, Object> variables;
     private final Map<String, FragmentDefinition> fragments;
     private final OperationDefinition operationDefinition;
+    private final ExecutionId executionId;
+    private final CacheControl cacheControl;
 
-    private ServiceExecutionParameters(Document query, Object context, Map<String, Object> variables, Map<String, FragmentDefinition> fragments, OperationDefinition operationDefinition) {
+    private ServiceExecutionParameters(Document query, Object context, Map<String, Object> variables, Map<String, FragmentDefinition> fragments, OperationDefinition operationDefinition, ExecutionId executionId, CacheControl cacheControl) {
         this.query = assertNotNull(query);
         this.variables = assertNotNull(variables);
         this.fragments = assertNotNull(fragments);
         this.operationDefinition = assertNotNull(operationDefinition);
         this.context = context;
+        this.executionId = executionId;
+        this.cacheControl = cacheControl;
     }
 
     public Document getQuery() {
@@ -34,6 +40,14 @@ public class ServiceExecutionParameters {
 
     public Object getContext() {
         return context;
+    }
+
+    public ExecutionId getExecutionId() {
+        return executionId;
+    }
+
+    public CacheControl getCacheControl() {
+        return cacheControl;
     }
 
     public Map<String, Object> getVariables() {
@@ -52,7 +66,6 @@ public class ServiceExecutionParameters {
         return operationDefinition.getOperation();
     }
 
-
     public static Builder newServiceExecutionParameters() {
         return new Builder();
     }
@@ -63,6 +76,8 @@ public class ServiceExecutionParameters {
         private Map<String, Object> variables = new LinkedHashMap<>();
         private Map<String, FragmentDefinition> fragments = new LinkedHashMap<>();
         private OperationDefinition operationDefinition;
+        private ExecutionId executionId;
+        private CacheControl cacheControl;
 
         private Builder() {
         }
@@ -74,6 +89,16 @@ public class ServiceExecutionParameters {
 
         public Builder context(Object context) {
             this.context = context;
+            return this;
+        }
+
+        public Builder executionId(ExecutionId executionId) {
+            this.executionId = executionId;
+            return this;
+        }
+
+        public Builder cacheControl(CacheControl cacheControl) {
+            this.cacheControl = cacheControl;
             return this;
         }
 
@@ -93,7 +118,7 @@ public class ServiceExecutionParameters {
         }
 
         public ServiceExecutionParameters build() {
-            return new ServiceExecutionParameters(query, context, variables, fragments, operationDefinition);
+            return new ServiceExecutionParameters(query, context, variables, fragments, operationDefinition, executionId, cacheControl);
         }
     }
 }
