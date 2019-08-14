77e4e2b6afb6d2a7f9bd483f6a92a8be05f76a21
Brad Baker <bbaker@atlassian.com>
2019-08-14 12:40:53 +1000

    Now sends in execution context to service Hooks

--- src/main/java/graphql/nadel/hooks/ServiceExecutionHooks.java
+++ src/main/java/graphql/nadel/hooks/ServiceExecutionHooks.java
@@ -1,5 +1,6 @@
 package graphql.nadel.hooks;
 
+import graphql.execution.ExecutionContext;
 import graphql.execution.ExecutionStepInfo;
 import graphql.execution.nextgen.result.RootExecutionResultNode;
 import graphql.nadel.Service;
@@ -16,12 +17,13 @@ public interface ServiceExecutionHooks {
     /**
      * Called per top level field that for a service.  This allows you to create a "context" object that will be passed into further calls.
      *
+     * @param executionCtx     the {@link graphql.execution.ExecutionContext} in play
      * @param service          the service in play
      * @param topLevelStepInfo the top level field execution step info
      *
      * @return an async context object of your choosing
      */
-    default CompletableFuture<Object> createServiceContext(Service service, ExecutionStepInfo topLevelStepInfo) {
+    default CompletableFuture<Object> createServiceContext(ExecutionContext executionCtx, Service service, ExecutionStepInfo topLevelStepInfo) {
         return CompletableFuture.completedFuture(null);
     }
 
@@ -29,7 +31,7 @@ public interface ServiceExecutionHooks {
      * Called to possibly change the arguments and runtime variables that are passed onto a called service
      *
      * @param service          the service in play
-     * @param serviceContext   the context object created in {@link #createServiceContext(graphql.nadel.Service, graphql.execution.ExecutionStepInfo)}
+     * @param serviceContext   the context object created in {@link #createServiceContext(graphql.execution.ExecutionContext, graphql.nadel.Service, graphql.execution.ExecutionStepInfo)}
      * @param topLevelStepInfo the top level field execution step info
      *
      * @return an async null to indicate NO change needed or an async new set of ModifiedArguments
@@ -42,7 +44,7 @@ public interface ServiceExecutionHooks {
      * Called to allow a service to post process the service result in some fashion.
      *
      * @param service        the service in play
-     * @param serviceContext the context object created in {@link #createServiceContext(graphql.nadel.Service, graphql.execution.ExecutionStepInfo)}
+     * @param serviceContext the context object created in {@link #createServiceContext(graphql.execution.ExecutionContext, graphql.nadel.Service, graphql.execution.ExecutionStepInfo)}
      * @param overallSchema  the overall schema
      * @param resultNode     the result
      *
