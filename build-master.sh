f5dfbf02fce9e2afcafc6c7d2dbb6131bfebf71f
andimarek <andimarek@fastmail.fm>
2019-12-19 14:44:07 +1100

    build testing

--- build-master.sh
+++ build-master.sh
@@ -3,4 +3,4 @@ set -ev
 echo "current git hash:"
 git rev-parse --short HEAD
 BUILD_COMMAND="./gradlew clean assemble && ./gradlew check  --info && ./gradlew publish -x check --info"
-docker run --rm -v `pwd .`:/build -e BINTRAY_USER=$BINTRAY_USER -e BINTRAY_API_KEY=$BINTRAY_API_KEY -e MAVEN_CENTRAL_USER=$MAVEN_CENTRAL_USER -e MAVEN_CENTRAL_PASSWORD=$MAVEN_CENTRAL_PASSWORD -w /build openjdk:8u131-jdk bash -c "${BUILD_COMMAND}"
+docker run --rm -v `pwd .`:/build -e GITHUB_TOKEN=$GITHUB_TOKEN -e BINTRAY_USER=$BINTRAY_USER -e BINTRAY_API_KEY=$BINTRAY_API_KEY -e MAVEN_CENTRAL_USER=$MAVEN_CENTRAL_USER -e MAVEN_CENTRAL_PASSWORD=$MAVEN_CENTRAL_PASSWORD -w /build openjdk:8u131-jdk bash -c "${BUILD_COMMAND}"
