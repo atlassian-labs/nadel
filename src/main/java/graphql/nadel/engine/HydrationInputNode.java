a8225a3500b2931014d1909a8d33db48f2c8946d
Franklin Wang <fractalemagic@gmail.com>
2020-02-21 17:16:07 +1100

    Fix hydration node elapsed time

--- src/main/java/graphql/nadel/engine/HydrationInputNode.java
+++ src/main/java/graphql/nadel/engine/HydrationInputNode.java
@@ -1,10 +1,14 @@
 package graphql.nadel.engine;
 
+import graphql.GraphQLError;
 import graphql.execution.ExecutionStepInfo;
 import graphql.execution.NonNullableFieldWasNullException;
 import graphql.execution.nextgen.result.ResolvedValue;
 import graphql.nadel.engine.transformation.HydrationTransformation;
+import graphql.nadel.result.ElapsedTime;
 import graphql.nadel.result.LeafExecutionResultNode;
+
+import java.util.List;
 
 public class HydrationInputNode extends LeafExecutionResultNode {
 
@@ -13,8 +17,10 @@ public class HydrationInputNode extends LeafExecutionResultNode {
     public HydrationInputNode(HydrationTransformation hydrationTransformation,
                               ExecutionStepInfo executionStepInfo,
                               ResolvedValue resolvedValue,
-                              NonNullableFieldWasNullException nonNullableFieldWasNullException) {
-        super(executionStepInfo, resolvedValue, nonNullableFieldWasNullException);
+                              NonNullableFieldWasNullException nonNullableFieldWasNullException,
+                              List<GraphQLError> errors,
+                              ElapsedTime elapsedTime) {
+        super(executionStepInfo, resolvedValue, nonNullableFieldWasNullException, errors, elapsedTime);
         this.hydrationTransformation = hydrationTransformation;
     }
 
