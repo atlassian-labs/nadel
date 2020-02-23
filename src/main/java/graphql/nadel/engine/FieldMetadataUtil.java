cb1003a5ddef7e51ef96de1967f7277e182de953
Franklin Wang <fractalemagic@gmail.com>
2020-02-24 10:31:30 +1100

    GQLGW-222: return empty collection instead of null

--- src/main/java/graphql/nadel/engine/FieldMetadataUtil.java
+++ src/main/java/graphql/nadel/engine/FieldMetadataUtil.java
@@ -56,27 +56,16 @@ public class FieldMetadataUtil {
 
     public static List<String> getRootOfTransformationIds(Field field) {
         List<FieldMetadata> fieldMetadata = readMetadata(field);
-        if (fieldMetadata == null) {
-            return Collections.emptyList();
-        }
-
         return FpKit.filterAndMap(fieldMetadata, FieldMetadata::isRootOfTransformation, FieldMetadata::getId);
     }
 
     public static List<String> getFieldIds(Field field) {
         List<FieldMetadata> fieldMetadata = readMetadata(field);
-        if (fieldMetadata == null) {
-            return Collections.emptyList();
-        }
         return graphql.util.FpKit.map(fieldMetadata, FieldMetadata::getId);
     }
 
     public static Field addFieldMetadata(Field field, String id, boolean rootOfTransformation) {
         List<FieldMetadata> fieldMetadata = readMetadata(field);
-        if (fieldMetadata == null) {
-            fieldMetadata = new ArrayList<>();
-        }
-
         FieldMetadata newFieldMetadata = new FieldMetadata(id, rootOfTransformation);
         fieldMetadata.add(newFieldMetadata);
         return writeMetadata(field, fieldMetadata);
@@ -84,19 +73,16 @@ public class FieldMetadataUtil {
 
     public static Field copyFieldMetadata(Field from, Field to) {
         List<FieldMetadata> fromMetadata = readMetadata(from);
-        if (fromMetadata == null || fromMetadata.isEmpty()) {
+        if (fromMetadata.isEmpty()) {
             return to;
         }
         List<FieldMetadata> toMetadata = readMetadata(to);
-        if (toMetadata == null) {
-            toMetadata = new ArrayList<>();
-        }
         toMetadata.addAll(fromMetadata);
         return writeMetadata(to, toMetadata);
     }
 
     public static String getUniqueRootFieldId(Field field) {
-        List<FieldMetadata> fieldMetadata = assertNotNull(readMetadata(field), "nadel field id expected");
+        List<FieldMetadata> fieldMetadata = readMetadata(field);
         List<FieldMetadata> rootFieldMetadata = filter(fieldMetadata, FieldMetadata::isRootOfTransformation);
         assertTrue(rootFieldMetadata.size() == 1, "exactly one root nadel infos expected");
         return rootFieldMetadata.get(0).id;
@@ -149,7 +135,7 @@ public class FieldMetadataUtil {
 
     private static List<FieldMetadata> readMetadata(Field field) {
         String serialized = field.getAdditionalData().get(NADEL_FIELD_METADATA);
-        return serialized != null ? readMetadata(serialized) : null;
+        return serialized != null ? readMetadata(serialized) : new ArrayList<>();
     }
 
     public static Field setOverallTypeInfoId(Field field, String id) {
