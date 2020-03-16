378f19305424a6617f5723b156f6e65a570a4d58
Faizan Siddiqui <fsiddiqui@atlassian.com>
2020-03-16 11:39:04 +1100

    added class specific tests for ResultComplexityAggregator

---
+++ src/test/groovy/graphql/nadel/result/ResultComplexityAggregatorTest.groovy
@@ -0,0 +1,88 @@
+package graphql.nadel.result
+
+
+import spock.lang.Specification
+
+class ResultComplexityAggregatorTest extends Specification {
+    def resultComplexityAggregator = new ResultComplexityAggregator();
+
+    def "test single service"() {
+        when:
+        resultComplexityAggregator.incrementServiceNodeCount("service1", 10)
+
+        then:
+        resultComplexityAggregator.getNodeCountsForService("service1") == 10
+        resultComplexityAggregator.getTotalNodeCount() == 10
+    }
+
+    def "test multiple services"() {
+        when:
+        resultComplexityAggregator.incrementServiceNodeCount("service1", 10)
+        resultComplexityAggregator.incrementServiceNodeCount("service2", 5)
+
+
+        then:
+        resultComplexityAggregator.getNodeCountsForService("service1") == 10
+        resultComplexityAggregator.getNodeCountsForService("service2") == 5
+
+        resultComplexityAggregator.getTotalNodeCount() == 15
+    }
+
+    def "test multiple adds to single service"() {
+        when:
+        resultComplexityAggregator.incrementServiceNodeCount("service1", 3)
+        resultComplexityAggregator.incrementServiceNodeCount("service1", 5)
+
+        then:
+        resultComplexityAggregator.getNodeCountsForService("service1") == 8
+        resultComplexityAggregator.getTotalNodeCount() == 8
+    }
+
+    def "test multiple adds to multiple service"() {
+        when:
+        resultComplexityAggregator.incrementServiceNodeCount("service1", 3)
+        resultComplexityAggregator.incrementServiceNodeCount("service2", 5)
+        resultComplexityAggregator.incrementServiceNodeCount("service1", 3)
+        resultComplexityAggregator.incrementServiceNodeCount("service1", 5)
+        resultComplexityAggregator.incrementServiceNodeCount("service1", 3)
+        resultComplexityAggregator.incrementServiceNodeCount("service2", 5)
+
+        then:
+        resultComplexityAggregator.getNodeCountsForService("service1") == 14
+        resultComplexityAggregator.getNodeCountsForService("service2") == 10
+        resultComplexityAggregator.getTotalNodeCount() == 24
+    }
+
+    def "test toString method"() {
+        when:
+        resultComplexityAggregator.incrementServiceNodeCount("service1", 3)
+        resultComplexityAggregator.incrementServiceNodeCount("service2", 5)
+        resultComplexityAggregator.incrementServiceNodeCount("service1", 3)
+        resultComplexityAggregator.incrementServiceNodeCount("service1", 5)
+        resultComplexityAggregator.incrementServiceNodeCount("service1", 3)
+        resultComplexityAggregator.incrementServiceNodeCount("service2", 5)
+
+        then:
+        resultComplexityAggregator.toString() == "ResultComplexityAggregator{totalNodeCount=24, serviceNodeCountsMap={service1=14, service2=10}}"
+    }
+
+    def "test snapshotComplexityData method"() {
+        when:
+        resultComplexityAggregator.incrementServiceNodeCount("service1", 3)
+        resultComplexityAggregator.incrementServiceNodeCount("service2", 5)
+        resultComplexityAggregator.incrementServiceNodeCount("service1", 3)
+        resultComplexityAggregator.incrementServiceNodeCount("service1", 5)
+        resultComplexityAggregator.incrementServiceNodeCount("service1", 3)
+        resultComplexityAggregator.incrementServiceNodeCount("service2", 5)
+
+        def complexityMap = resultComplexityAggregator.snapshotResultComplexityData()
+
+        then:
+        complexityMap.toString() == "[totalNodeCount:24, serviceNodeCounts:[service1:14, service2:10]]"
+    }
+
+
+
+
+
+}
