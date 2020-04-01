de9f458b3afbe4e38dc0186459b5fd1373fd1c2f
andimarek <andimarek@fastmail.fm>
2020-04-01 20:16:31 +1100

    cleanup

--- src/main/java/graphql/nadel/engine/transformation/Metadata.java
+++ src/main/java/graphql/nadel/engine/transformation/Metadata.java
@@ -1,24 +1,18 @@
 package graphql.nadel.engine.transformation;
 
 import graphql.GraphQLError;
-import graphql.execution.ExecutionPath;
 import graphql.nadel.normalized.NormalizedQueryField;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class Metadata {
 
     private List<NormalizedFieldAndError> removedFields = new ArrayList<>();
 
     private Map<String, List<FieldMetadata>> metadataByFieldId = new LinkedHashMap<>();
-
-    private final Set<ExecutionPath> processedTransformation = Collections.synchronizedSet(new LinkedHashSet<>());
 
     public static class NormalizedFieldAndError {
         private final NormalizedQueryField normalizedField;
@@ -58,14 +52,5 @@ public class Metadata {
         return metadataByFieldId;
     }
 
-    public void addProcessedTransformation(ExecutionPath executionPath) {
-//        System.out.println("processed path: " + executionPath);
-        processedTransformation.add(executionPath);
-    }
-
-    public boolean isTransformationProcessed(ExecutionPath path) {
-//        System.out.println(" checking : " + path);
-        return processedTransformation.contains(path);
-    }
 }
 
