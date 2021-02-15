4875f7ab7bec958733ead69ab2d5cd40da01f10d
dondonz <13839920+dondonz@users.noreply.github.com>
2021-02-15 15:41:30 +1100

    GQLGW-697 Add NadelExecutionHints

---
+++ src/main/java/graphql/nadel/NadelExecutionHints.java
@@ -0,0 +1,33 @@
+package graphql.nadel;
+
+public class NadelExecutionHints {
+    private final boolean optimizeExecution;
+
+    private NadelExecutionHints(boolean optimizeExecution) {
+        this.optimizeExecution = optimizeExecution;
+    }
+
+    public static Builder newHints() {
+        return new Builder();
+    }
+
+    public boolean getOptimizeExecutionFlag() {
+        return optimizeExecution;
+    }
+
+    public static class Builder {
+        private boolean optimizeExecution;
+
+        private Builder() {
+        }
+
+        public Builder optimizeExecution(boolean flag) {
+            this.optimizeExecution = flag;
+            return this;
+        }
+
+        public NadelExecutionHints build() {
+            return new NadelExecutionHints(optimizeExecution);
+        }
+    }
+}
