78de51e91d92cc0c4ffa44c771124289520a07cf
Franklin Wang <fractalemagic@gmail.com>
2021-09-09 13:13:44 +1200

    Try fix buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,9 @@
+repositories {
+    mavenCentral()
+    maven(url = "https://packages.atlassian.com/private-snapshot")
+    mavenLocal()
+}
+
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.4.31"
 }
