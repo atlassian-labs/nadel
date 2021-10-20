dca6a43c6dba6872005b9545721f740647d16d46
Franklin Wang <fractalemagic@gmail.com>
2021-10-21 12:48:31 +1300

    Upgrade Kotlin to 1.5

--- engine-nextgen/build.gradle.kts
+++ engine-nextgen/build.gradle.kts
@@ -9,8 +9,8 @@ dependencies {
 
     api(kotlin("stdlib"))
     api(kotlin("reflect"))
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.4.2")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.5.2")
 }
 
 tasks.withType<KotlinCompile> {
