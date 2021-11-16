c7fb2ce14ed0dffa48003f6526c8369ece3447f2
Brad Baker <bbaker@atlassian.com>
2021-11-16 16:31:02 +1100

    GQLGW-530 - merged in depedant branch and also fixed test

--- engine/src/test/groovy/graphql/nadel/engine/ServiceExecutionParametersTest.groovy
+++ engine/src/test/groovy/graphql/nadel/engine/ServiceExecutionParametersTest.groovy
@@ -4,15 +4,10 @@ import graphql.cachecontrol.CacheControl
 import graphql.execution.ExecutionId
 import graphql.language.Document
 import graphql.language.OperationDefinition
-import graphql.language.SourceLocation
 import graphql.nadel.ServiceExecutionHydrationDetails
 import graphql.nadel.ServiceExecutionParameters
-import graphql.nadel.dsl.UnderlyingServiceHydration
 import graphql.nadel.engine.testutils.TestUtil
 import spock.lang.Specification
-
-import static java.util.Collections.emptyList
-import static java.util.Collections.emptyMap
 
 class ServiceExecutionParametersTest extends Specification {
 
@@ -24,22 +19,7 @@ class ServiceExecutionParametersTest extends Specification {
         def variables = [variables: "okPresent"]
         def executionId = ExecutionId.generate()
         def cacheControl = CacheControl.newCacheControl()
-        def hydrationAst = new UnderlyingServiceHydration(
-                new SourceLocation(0, 0),
-                emptyList(),
-                "Service",
-                "Query",
-                "test",
-                emptyList(),
-                "id",
-                false,
-                false,
-                5,
-                10,
-                emptyMap()
-        )
-
-        def hydrationDetails = new ServiceExecutionHydrationDetails(hydrationAst)
+        def hydrationDetails = new ServiceExecutionHydrationDetails(10,50)
 
         when:
         def parameters = ServiceExecutionParameters.newServiceExecutionParameters()
@@ -60,5 +40,6 @@ class ServiceExecutionParametersTest extends Specification {
         parameters.cacheControl == cacheControl
         parameters.hydrationCall
         parameters.hydrationDetails.getTimeout() == 10
+        parameters.hydrationDetails.getBatchSize() == 50
     }
 }
