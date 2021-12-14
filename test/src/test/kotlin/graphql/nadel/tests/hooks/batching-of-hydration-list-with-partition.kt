044063c24b9d4d9a3b4405299659347ebc9d10fd
Artyom Emelyanenko <tema.emelyan@gmail.com>
2021-12-14 12:04:15 +1100

    fix broken tests after merge

--- test/src/test/kotlin/graphql/nadel/tests/hooks/batching-of-hydration-list-with-partition.kt
+++ test/src/test/kotlin/graphql/nadel/tests/hooks/batching-of-hydration-list-with-partition.kt
@@ -4,6 +4,7 @@ import graphql.nadel.Nadel
 import graphql.nadel.enginekt.NadelEngineExecutionHooks
 import graphql.nadel.enginekt.blueprint.NadelBatchHydrationFieldInstruction
 import graphql.nadel.enginekt.blueprint.NadelGenericHydrationInstruction
+import graphql.nadel.enginekt.transform.artificial.NadelAliasHelper
 import graphql.nadel.enginekt.transform.result.json.JsonNode
 import graphql.nadel.tests.EngineTestHook
 import graphql.nadel.tests.NadelEngineType
@@ -12,7 +13,8 @@ import graphql.nadel.tests.UseHook
 private class BatchHydrationHooks : NadelEngineExecutionHooks {
     override fun <T : NadelGenericHydrationInstruction> getHydrationInstruction(
         instructions: List<T>,
-        parentNode: JsonNode
+        parentNode: JsonNode,
+        aliasHelper: NadelAliasHelper
     ): T {
         return instructions[0]
     }
