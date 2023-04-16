cd66d0a10a0a983f216c99ff38aa8653778c3ed8
Brad Baker <bbaker@atlassian.com>
2023-04-16 12:02:32 +1000

    GQLGW-666 Added graphql context to Nadel

--- lib/src/main/java/graphql/nadel/Nadel.kt
+++ lib/src/main/java/graphql/nadel/Nadel.kt
@@ -53,6 +53,14 @@ class Nadel private constructor(
             .query(nadelExecutionInput.query)
             .operationName(nadelExecutionInput.operationName)
             .context(nadelExecutionInput.context)
+            .graphQLContext { builder ->
+                // we need to transfer over all the keys - ExecutionInput does not allow for setting it direct
+                run {
+                    nadelExecutionInput.graphqlContext.stream().forEach {
+                        builder.put(it.key, it.value)
+                    }
+                }
+            }
             .variables(nadelExecutionInput.variables)
             .extensions(nadelExecutionInput.extensions)
             .executionId(nadelExecutionInput.executionId)
