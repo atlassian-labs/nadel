feeed8dee74575e3a06330c1ade62fab76106056
Steven Barker <sbarker2@atlassian.com>
2024-03-05 10:13:12 +1300

    fix breaking changes from graphql-java version upgrade

--- lib/src/main/java/graphql/nadel/ServiceExecutionResult.kt
+++ lib/src/main/java/graphql/nadel/ServiceExecutionResult.kt
@@ -3,13 +3,13 @@ package graphql.nadel
 import graphql.incremental.DelayedIncrementalPartialResult
 import org.reactivestreams.Publisher
 
-sealed class ServiceExecutionResult @JvmOverloads constructor(
+open class ServiceExecutionResult @JvmOverloads constructor(
     val data: MutableMap<String, Any?> = LinkedHashMap(),
     val errors: MutableList<MutableMap<String, Any?>> = ArrayList(),
     val extensions: MutableMap<String, Any?> = LinkedHashMap(),
 )
 
-class NadelIncrementalServiceExecutionResult(
+open class NadelIncrementalServiceExecutionResult(
     serviceExecutionResult: ServiceExecutionResult,
     val incrementalItemPublisher: Publisher<DelayedIncrementalPartialResult>,
 ) : ServiceExecutionResult(serviceExecutionResult.data, serviceExecutionResult.errors, serviceExecutionResult.extensions)
