6a4a3daaf981063ebb268133e3b57a093762e8e5
Steven Barker <sbarker2@atlassian.com>
2024-03-04 13:46:55 +1300

    add NadelIncrementalServiceExecutionResult

--- lib/src/main/java/graphql/nadel/ServiceExecutionResult.kt
+++ lib/src/main/java/graphql/nadel/ServiceExecutionResult.kt
@@ -1,7 +1,16 @@
 package graphql.nadel
 
-open class ServiceExecutionResult @JvmOverloads constructor(
+import graphql.incremental.DelayedIncrementalPartialResult
+import org.reactivestreams.Publisher
+
+sealed class ServiceExecutionResult @JvmOverloads constructor(
     val data: MutableMap<String, Any?> = LinkedHashMap(),
     val errors: MutableList<MutableMap<String, Any?>> = ArrayList(),
     val extensions: MutableMap<String, Any?> = LinkedHashMap(),
 )
+
+class NadelIncrementalServiceExecutionResult(
+    serviceExecutionResult: ServiceExecutionResult,
+    val incrementalItemPublisher: Publisher<DelayedIncrementalPartialResult>,
+) : ServiceExecutionResult(serviceExecutionResult.data, serviceExecutionResult.errors, serviceExecutionResult.extensions)
+
