16402af269496437a3052f38fbb064d8db529b65
Steven Barker <sbarker2@atlassian.com>
2024-03-05 11:22:22 +1300

    changing ServiceExecutionResult to a sealed class

--- lib/src/main/java/graphql/nadel/ServiceExecutionResult.kt
+++ lib/src/main/java/graphql/nadel/ServiceExecutionResult.kt
@@ -3,7 +3,7 @@ package graphql.nadel
 import graphql.incremental.DelayedIncrementalPartialResult
 import org.reactivestreams.Publisher
 
-open class ServiceExecutionResult @JvmOverloads constructor(
+sealed class ServiceExecutionResult @JvmOverloads constructor(
     val data: MutableMap<String, Any?> = LinkedHashMap(),
     val errors: MutableList<MutableMap<String, Any?>> = ArrayList(),
     val extensions: MutableMap<String, Any?> = LinkedHashMap(),
