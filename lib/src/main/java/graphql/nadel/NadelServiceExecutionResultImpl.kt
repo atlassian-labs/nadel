c493c230859007c1456e5d885485b80d4e286b52
Steven Barker <sbarker2@atlassian.com>
2024-03-05 17:08:25 +1300

    add hasNext to IncrementalServiceExecutionResult

--- lib/src/main/java/graphql/nadel/NadelServiceExecutionResultImpl.kt
+++
@@ -1,6 +0,0 @@
-package graphql.nadel
-open class NadelServiceExecutionResultImpl(
-    data: MutableMap<String, Any?> = LinkedHashMap(),
-    errors: MutableList<MutableMap<String, Any?>> = ArrayList(),
-    extensions: MutableMap<String, Any?> = LinkedHashMap(),
-) : ServiceExecutionResult(data, errors, extensions)
--- lib/src/main/java/graphql/nadel/ServiceExecutionResult.kt
+++ lib/src/main/java/graphql/nadel/ServiceExecutionResult.kt
@@ -12,4 +12,11 @@ sealed class ServiceExecutionResult @JvmOverloads constructor(
 class NadelIncrementalServiceExecutionResult(
     serviceExecutionResult: ServiceExecutionResult,
     val incrementalItemPublisher: Publisher<DelayedIncrementalPartialResult>,
+    val hasNext: Boolean,
 ) : ServiceExecutionResult(serviceExecutionResult.data, serviceExecutionResult.errors, serviceExecutionResult.extensions)
+
+class NadelServiceExecutionResultImpl(
+    data: MutableMap<String, Any?> = LinkedHashMap(),
+    errors: MutableList<MutableMap<String, Any?>> = ArrayList(),
+    extensions: MutableMap<String, Any?> = LinkedHashMap(),
+) : ServiceExecutionResult(data, errors, extensions)
