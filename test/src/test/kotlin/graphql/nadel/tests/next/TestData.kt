5f361fe3b9f19fc6490eca80c02d05063799a5fd
Franklin Wang <fwang@atlassian.com>
2024-05-02 09:55:34 +1000

    Mess around with real GraphQL in a test scenario

---
+++ test/src/test/kotlin/graphql/nadel/tests/next/TestData.kt
@@ -0,0 +1,24 @@
+package graphql.nadel.tests.next
+
+import org.intellij.lang.annotations.Language
+
+abstract class TestData {
+    abstract val calls: List<ExpectedServiceCall>
+    abstract val response: ExpectedNadelResponse
+}
+
+data class ExpectedServiceCall(
+    @Language("GraphQL")
+    val query: String,
+    @Language("JSON")
+    val variables: String,
+    @Language("JSON")
+    val response: String,
+    val delayedResponses: List<String>,
+)
+
+data class ExpectedNadelResponse(
+    @Language("JSON")
+    val response: String?,
+    val delayedResponses: List<String>,
+)
---
+++ test/src/test/kotlin/graphql/nadel/tests/next/fixtures/EchoTestData.kt
@@ -0,0 +1,51 @@
+// @formatter:off
+package graphql.nadel.tests.next.fixtures
+
+import graphql.nadel.tests.next.ExpectedNadelResponse
+import graphql.nadel.tests.next.ExpectedServiceCall
+import graphql.nadel.tests.next.TestData
+import graphql.nadel.tests.next.listOfJsonStrings
+import kotlin.Suppress
+import kotlin.collections.List
+import kotlin.collections.listOf
+
+/**
+ * This class is generated. Do NOT modify.
+ *
+ * Refer to [graphql.nadel.tests.next.CaptureTestData]
+ */
+@Suppress("unused")
+public class EchoTestData : TestData() {
+    override val calls: List<ExpectedServiceCall> = listOf(
+                ExpectedServiceCall(
+                    query = """
+                    | {
+                    |   echo
+                    | }
+                    """.trimMargin(),
+                    variables = "{}",
+                    response = """
+                    | {
+                    |   "data": {
+                    |     "echo": "Hello World"
+                    |   }
+                    | }
+                    """.trimMargin(),
+                    delayedResponses = listOfJsonStrings(
+                    ),
+                ),
+        )
+
+    override val response: ExpectedNadelResponse = ExpectedNadelResponse(
+            response = """
+            | {
+            |   "data": {
+            |     "echo": "Hello World"
+            |   },
+            |   "hasNext": true
+            | }
+            """.trimMargin(),
+            delayedResponses = listOfJsonStrings(
+            ),
+        )
+}
