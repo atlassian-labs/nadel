bc37fa566ba23ba02224deac1cd5e71908fa0957
Franklin Wang <fwang@atlassian.com>
2024-05-01 15:53:29 +1000

    Undo changes

--- lib/src/main/java/graphql/nadel/Service.kt
+++ lib/src/main/java/graphql/nadel/Service.kt
@@ -18,5 +18,4 @@ open class Service(
      * These are the GraphQL definitions that a service contributes to the OVERALL schema.
      */
     val definitionRegistry: NadelDefinitionRegistry,
-    val underlyingTypeDefinitions: TypeDefinitionRegistry? = null,
 )
