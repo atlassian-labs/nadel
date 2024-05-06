e0ca1641a98e17625f4be2cd21c67ef082c285bc
Franklin Wang <fwang@atlassian.com>
2024-05-06 13:20:53 +1200

    More tests or something

---
+++ test/src/test/kotlin/graphql/nadel/tests/next/fixtures/hydration/defer/HydrationDeferInRenamedFieldUsingRenamedFieldTestData.kt
@@ -0,0 +1,114 @@
+// @formatter:off
+package graphql.nadel.tests.next.fixtures.hydration.defer
+
+import graphql.nadel.tests.next.ExpectedNadelResponse
+import graphql.nadel.tests.next.ExpectedServiceCall
+import graphql.nadel.tests.next.TestData
+import graphql.nadel.tests.next.listOfJsonStrings
+import kotlin.Suppress
+import kotlin.collections.List
+import kotlin.collections.listOf
+
+/**
+ * This class is generated. Do NOT modify.
+ *
+ * Refer to [graphql.nadel.tests.next.CaptureTestData]
+ */
+@Suppress("unused")
+public class HydrationDeferInRenamedFieldUsingRenamedFieldTestData : TestData() {
+    override val calls: List<ExpectedServiceCall> = listOf(
+            ExpectedServiceCall(
+                service = "issues",
+                query = """
+                | {
+                |   rename__issueByKey__getIssueByKey: getIssueByKey(key: "GQLGW-1") {
+                |     key
+                |     hydration__assigneeV2__assigneeId: assigneeId
+                |     __typename__hydration__assigneeV2: __typename
+                |   }
+                | }
+                """.trimMargin(),
+                variables = "{}",
+                response = """
+                | {
+                |   "rename__issueByKey__getIssueByKey": {
+                |     "key": "GQLGW-1",
+                |     "hydration__assigneeV2__assigneeId": "ari:cloud:identity::user/1",
+                |     "__typename__hydration__assigneeV2": "Issue"
+                |   }
+                | }
+                """.trimMargin(),
+                delayedResponses = listOfJsonStrings(
+                ),
+            ),
+            ExpectedServiceCall(
+                service = "users",
+                query = """
+                | {
+                |   rename__quickUser__user_fast: user_fast(id: "ari:cloud:identity::user/1") {
+                |     name
+                |   }
+                | }
+                """.trimMargin(),
+                variables = "{}",
+                response = """
+                | {
+                |   "rename__quickUser__user_fast": {
+                |     "name": "SPEED"
+                |   }
+                | }
+                """.trimMargin(),
+                delayedResponses = listOfJsonStrings(
+                ),
+            ),
+        )
+
+    /**
+     * ```json
+     * {
+     *   "data": {
+     *     "issueByKey": {
+     *       "key": "GQLGW-1",
+     *       "assigneeV2": {
+     *         "value": {
+     *           "name": "SPEED"
+     *         }
+     *       }
+     *     }
+     *   }
+     * }
+     * ```
+     */
+    override val response: ExpectedNadelResponse = ExpectedNadelResponse(
+            response = """
+            | {
+            |   "data": {
+            |     "issueByKey": {
+            |       "key": "GQLGW-1"
+            |     }
+            |   },
+            |   "hasNext": true
+            | }
+            """.trimMargin(),
+            delayedResponses = listOfJsonStrings(
+                """
+                | {
+                |   "hasNext": false,
+                |   "incremental": [
+                |     {
+                |       "path": [
+                |         "issueByKey",
+                |         "assigneeV2"
+                |       ],
+                |       "data": {
+                |         "value": {
+                |           "name": "SPEED"
+                |         }
+                |       }
+                |     }
+                |   ]
+                | }
+                """.trimMargin(),
+            ),
+        )
+}
