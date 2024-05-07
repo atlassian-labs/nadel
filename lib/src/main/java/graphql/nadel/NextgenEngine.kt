fff7b709999b65913cfd012cf8f7efa28a38c82d
Steven Barker <sbarker2@atlassian.com>
2024-05-07 16:54:36 +1200

    merge incremental results

--- lib/src/main/java/graphql/nadel/NextgenEngine.kt
+++ lib/src/main/java/graphql/nadel/NextgenEngine.kt
@@ -1,11 +1,13 @@
 package graphql.nadel
 
+import com.sun.tools.javac.comp.Flow
 import graphql.ErrorType
 import graphql.ExecutionInput
 import graphql.ExecutionResult
 import graphql.GraphQLError
 import graphql.execution.ExecutionIdProvider
 import graphql.execution.instrumentation.InstrumentationState
+import graphql.incremental.DelayedIncrementalPartialResult
 import graphql.incremental.IncrementalExecutionResultImpl
 import graphql.language.Document
 import graphql.nadel.engine.NadelIncrementalResultSupport
@@ -318,6 +320,12 @@ internal class NextgenEngine(
                         ),
                     ).toSpecification(),
                 ),
+            )
+        }
+        
+        if (serviceExecResult is NadelIncrementalServiceExecutionResult) {
+            executionContext.deferSupport.defer(
+                serviceExecResult.incrementalItemPublisher.asFlow()
             )
         }
 
