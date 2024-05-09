18c83101e3f43d8a12999af62d8ee019f845a121
Franklin Wang <fwang@atlassian.com>
2024-05-09 14:10:09 +1200

    Improvements + refactoring after benchmarking

--- lib/src/main/java/graphql/nadel/result/NadelResultPathSegment.kt
+++ lib/src/main/java/graphql/nadel/result/NadelResultPathSegment.kt
@@ -5,5 +5,13 @@ internal sealed interface NadelResultPathSegment {
     value class Object(val key: String) : NadelResultPathSegment
 
     @JvmInline
-    value class Array(val index: Int) : NadelResultPathSegment
+    value class Array private constructor(val index: Int) : NadelResultPathSegment {
+        companion object {
+            private val cache = List(2000, ::Array)
+
+            operator fun invoke(index: Int): Array {
+                return cache.getOrNull(index) ?: Array(index)
+            }
+        }
+    }
 }
