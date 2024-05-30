f7524834ed678c2a797fb2e94fe964da7dd4f8c9
Franklin Wang <9077461+gnawf@users.noreply.github.com>
2024-05-30 15:28:38 +1000

    Instrumentation integration tests (#539)

---
+++ test/src/test/kotlin/graphql/nadel/tests/next/fixtures/instrumentation/InstrumentationBeginQueryExecutionOnCompleteOnFailureTestSnapshot.kt
@@ -0,0 +1,62 @@
+// @formatter:off
+package graphql.nadel.tests.next.fixtures.instrumentation
+
+import graphql.nadel.tests.next.ExpectedNadelResult
+import graphql.nadel.tests.next.ExpectedServiceCall
+import graphql.nadel.tests.next.TestSnapshot
+import graphql.nadel.tests.next.listOfJsonStrings
+import kotlin.Suppress
+import kotlin.collections.List
+import kotlin.collections.listOf
+
+/**
+ * This class is generated. Do NOT modify.
+ *
+ * Refer to [graphql.nadel.tests.next.UpdateTestSnapshots
+ */
+@Suppress("unused")
+public class InstrumentationBeginQueryExecutionOnCompleteOnFailureTestSnapshot : TestSnapshot() {
+    override val calls: List<ExpectedServiceCall> = listOf(
+            )
+
+    /**
+     * ```json
+     * {
+     *   "errors": [
+     *     {
+     *       "message": "An exception occurred invoking the service 'tester': no-op",
+     *       "locations": [],
+     *       "extensions": {
+     *         "executionId": "stable-id",
+     *         "classification": "DataFetchingException"
+     *       }
+     *     }
+     *   ],
+     *   "data": {
+     *     "echo": null
+     *   }
+     * }
+     * ```
+     */
+    override val result: ExpectedNadelResult = ExpectedNadelResult(
+            result = """
+            | {
+            |   "errors": [
+            |     {
+            |       "message": "An exception occurred invoking the service 'tester': no-op",
+            |       "locations": [],
+            |       "extensions": {
+            |         "executionId": "stable-id",
+            |         "classification": "DataFetchingException"
+            |       }
+            |     }
+            |   ],
+            |   "data": {
+            |     "echo": null
+            |   }
+            | }
+            """.trimMargin(),
+            delayedResults = listOfJsonStrings(
+            ),
+        )
+}
---
+++ test/src/test/kotlin/graphql/nadel/tests/next/fixtures/instrumentation/InstrumentationBeginQueryExecutionOnCompleteOnSuccessTestSnapshot.kt
@@ -0,0 +1,60 @@
+// @formatter:off
+package graphql.nadel.tests.next.fixtures.instrumentation
+
+import graphql.nadel.tests.next.ExpectedNadelResult
+import graphql.nadel.tests.next.ExpectedServiceCall
+import graphql.nadel.tests.next.TestSnapshot
+import graphql.nadel.tests.next.listOfJsonStrings
+import kotlin.Suppress
+import kotlin.collections.List
+import kotlin.collections.listOf
+
+/**
+ * This class is generated. Do NOT modify.
+ *
+ * Refer to [graphql.nadel.tests.next.UpdateTestSnapshots
+ */
+@Suppress("unused")
+public class InstrumentationBeginQueryExecutionOnCompleteOnSuccessTestSnapshot : TestSnapshot() {
+    override val calls: List<ExpectedServiceCall> = listOf(
+            ExpectedServiceCall(
+                service = "tester",
+                query = """
+                | {
+                |   echo
+                | }
+                """.trimMargin(),
+                variables = "{}",
+                result = """
+                | {
+                |   "data": {
+                |     "echo": "Hello World"
+                |   }
+                | }
+                """.trimMargin(),
+                delayedResults = listOfJsonStrings(
+                ),
+            ),
+        )
+
+    /**
+     * ```json
+     * {
+     *   "data": {
+     *     "echo": "Hello World"
+     *   }
+     * }
+     * ```
+     */
+    override val result: ExpectedNadelResult = ExpectedNadelResult(
+            result = """
+            | {
+            |   "data": {
+            |     "echo": "Hello World"
+            |   }
+            | }
+            """.trimMargin(),
+            delayedResults = listOfJsonStrings(
+            ),
+        )
+}
