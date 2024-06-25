4b43280ff0db48312b25d12307d3c3c65d3f53bd
sbarker2 <109047597+sbarker2@users.noreply.github.com>
2024-06-25 23:47:49 +1200

    ignore location field in defer test errors (#554)

--- test/src/test/kotlin/graphql/nadel/tests/next/fixtures/defer/DeferWithErrorTest.kt
+++ test/src/test/kotlin/graphql/nadel/tests/next/fixtures/defer/DeferWithErrorTest.kt
@@ -6,7 +6,6 @@ import graphql.nadel.NadelExecutionHints
 import graphql.nadel.tests.next.NadelIntegrationTest
 import kotlin.test.Ignore
 
-@Ignore
 open class DeferWithErrorTest : NadelIntegrationTest(
     query = """
       query {
