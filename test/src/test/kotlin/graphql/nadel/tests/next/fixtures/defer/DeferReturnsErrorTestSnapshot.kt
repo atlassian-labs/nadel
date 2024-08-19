6f25a6019e6eb13dd9ddc0760191ce6313fb6b97
sbarker2 <109047597+sbarker2@users.noreply.github.com>
2024-08-19 12:11:05 +1200

    add error test for defer (#574)

---
+++ test/src/test/kotlin/graphql/nadel/tests/next/fixtures/defer/DeferReturnsErrorTestSnapshot.kt
@@ -0,0 +1,110 @@
+// @formatter:off
+package graphql.nadel.tests.next.fixtures.defer
+
+import graphql.nadel.tests.next.ExpectedNadelResult
+import graphql.nadel.tests.next.ExpectedServiceCall
+import graphql.nadel.tests.next.TestSnapshot
+import graphql.nadel.tests.next.listOfJsonStrings
+import kotlin.Suppress
+import kotlin.collections.List
+import kotlin.collections.listOf
+
+private suspend fun main() {
+    graphql.nadel.tests.next.update<DeferReturnsErrorTest>()
+}
+
+/**
+ * This class is generated. Do NOT modify.
+ *
+ * Refer to [graphql.nadel.tests.next.UpdateTestSnapshots
+ */
+@Suppress("unused")
+public class DeferReturnsErrorTestSnapshot : TestSnapshot() {
+    override val calls: List<ExpectedServiceCall> = listOf(
+            ExpectedServiceCall(
+                service = "defer",
+                query = """
+                | {
+                |   defer {
+                |     hello
+                |     ... @defer(label: "slow-defer") {
+                |       slow
+                |     }
+                |   }
+                | }
+                """.trimMargin(),
+                variables = "{}",
+                result = """
+                | {
+                |   "data": {
+                |     "defer": {
+                |       "hello": "helloString"
+                |     }
+                |   },
+                |   "hasNext": true
+                | }
+                """.trimMargin(),
+                delayedResults = listOfJsonStrings(
+                    """
+                    | {
+                    |   "hasNext": false,
+                    |   "incremental": [
+                    |     {
+                    |       "path": [
+                    |         "defer"
+                    |       ],
+                    |       "label": "slow-defer",
+                    |       "data": {
+                    |         "slow": "java.lang.RuntimeException: An error occurred while fetching 'slow'"
+                    |       }
+                    |     }
+                    |   ]
+                    | }
+                    """.trimMargin(),
+                ),
+            ),
+        )
+
+    /**
+     * ```json
+     * {
+     *   "data": {
+     *     "defer": {
+     *       "hello": "helloString",
+     *       "slow": "java.lang.RuntimeException: An error occurred while fetching 'slow'"
+     *     }
+     *   }
+     * }
+     * ```
+     */
+    override val result: ExpectedNadelResult = ExpectedNadelResult(
+            result = """
+            | {
+            |   "data": {
+            |     "defer": {
+            |       "hello": "helloString"
+            |     }
+            |   },
+            |   "hasNext": true
+            | }
+            """.trimMargin(),
+            delayedResults = listOfJsonStrings(
+                """
+                | {
+                |   "hasNext": false,
+                |   "incremental": [
+                |     {
+                |       "path": [
+                |         "defer"
+                |       ],
+                |       "label": "slow-defer",
+                |       "data": {
+                |         "slow": "java.lang.RuntimeException: An error occurred while fetching 'slow'"
+                |       }
+                |     }
+                |   ]
+                | }
+                """.trimMargin(),
+            ),
+        )
+}
