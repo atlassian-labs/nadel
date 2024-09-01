eccaf0d67453dc6b99012077c0fe36289e4b72aa
sbarker2 <109047597+sbarker2@users.noreply.github.com>
2024-09-02 09:29:12 +1000

    Adding tests for the other transforms with defer (#582)
    * add deep deferred rename transform test
    
    * add tests for other transforms
    
    * add defer to deep rename transform test

---
+++ test/src/test/kotlin/graphql/nadel/tests/next/fixtures/defer/transforms/DeferredDeepRenameTestSnapshot.kt
@@ -0,0 +1,103 @@
+// @formatter:off
+package graphql.nadel.tests.next.fixtures.defer.transforms
+
+import graphql.nadel.tests.next.ExpectedNadelResult
+import graphql.nadel.tests.next.ExpectedServiceCall
+import graphql.nadel.tests.next.TestSnapshot
+import graphql.nadel.tests.next.listOfJsonStrings
+import kotlin.Suppress
+import kotlin.collections.List
+import kotlin.collections.listOf
+
+private suspend fun main() {
+    graphql.nadel.tests.next.update<DeferredDeepRenameTest>()
+}
+
+/**
+ * This class is generated. Do NOT modify.
+ *
+ * Refer to [graphql.nadel.tests.next.UpdateTestSnapshots
+ */
+@Suppress("unused")
+public class DeferredDeepRenameTestSnapshot : TestSnapshot() {
+    override val calls: List<ExpectedServiceCall> = listOf(
+            ExpectedServiceCall(
+                service = "defer",
+                query = """
+                | {
+                |   ... @defer {
+                |     details {
+                |       deep_rename__name__issue: issue {
+                |         name
+                |       }
+                |       __typename__deep_rename__name: __typename
+                |     }
+                |   }
+                | }
+                """.trimMargin(),
+                variables = "{}",
+                result = """
+                | {
+                |   "data": {},
+                |   "hasNext": true
+                | }
+                """.trimMargin(),
+                delayedResults = listOfJsonStrings(
+                    """
+                    | {
+                    |   "hasNext": false,
+                    |   "incremental": [
+                    |     {
+                    |       "path": [],
+                    |       "data": {
+                    |         "details": {
+                    |           "name": "Issue-1"
+                    |         }
+                    |       }
+                    |     }
+                    |   ]
+                    | }
+                    """.trimMargin(),
+                ),
+            ),
+        )
+
+    /**
+     * ```json
+     * {
+     *   "data": {
+     *     "details": {
+     *       "name": "Issue-1"
+     *     }
+     *   }
+     * }
+     * ```
+     */
+    override val result: ExpectedNadelResult = ExpectedNadelResult(
+            result = """
+            | {
+            |   "data": {
+            |     "details": null
+            |   },
+            |   "hasNext": true
+            | }
+            """.trimMargin(),
+            delayedResults = listOfJsonStrings(
+                """
+                | {
+                |   "hasNext": false,
+                |   "incremental": [
+                |     {
+                |       "path": [],
+                |       "data": {
+                |         "details": {
+                |           "name": "Issue-1"
+                |         }
+                |       }
+                |     }
+                |   ]
+                | }
+                """.trimMargin(),
+            ),
+        )
+}
