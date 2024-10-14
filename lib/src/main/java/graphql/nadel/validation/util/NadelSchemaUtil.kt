931cf955b5c20b94a2f057c3824328c497ff3c76
Felipe Reis <felipe.gdr@gmail.com>
2024-10-14 14:55:34 +1100

    Add validation for partition directive

--- lib/src/main/java/graphql/nadel/validation/util/NadelSchemaUtil.kt
+++ lib/src/main/java/graphql/nadel/validation/util/NadelSchemaUtil.kt
@@ -1,12 +1,16 @@
 package graphql.nadel.validation.util
 
 import graphql.language.FieldDefinition
+import graphql.language.OperationDefinition
 import graphql.nadel.Service
 import graphql.nadel.dsl.FieldMappingDefinition
 import graphql.nadel.dsl.NadelHydrationDefinition
+import graphql.nadel.engine.util.operationTypes
 import graphql.nadel.schema.NadelDirectives
+import graphql.nadel.util.NamespacedUtil
 import graphql.schema.GraphQLDirectiveContainer
 import graphql.schema.GraphQLFieldDefinition
+import graphql.schema.GraphQLNamedOutputType
 import graphql.schema.GraphQLNamedType
 import graphql.schema.GraphQLSchema
 
@@ -48,5 +52,19 @@ object NadelSchemaUtil {
     fun getRenamedFrom(type: GraphQLNamedType): String? {
         val asDirectivesContainer = type as? GraphQLDirectiveContainer ?: return null
         return NadelDirectives.createTypeMapping(asDirectivesContainer)?.underlyingName
+    }
+
+    fun hasPartition(field: GraphQLFieldDefinition): Boolean {
+        return hasPartition(field.definition!!)
+    }
+
+    fun hasPartition(def: FieldDefinition): Boolean {
+        return def.hasDirective(NadelDirectives.partitionDirectiveDefinition.name)
+    }
+
+    fun isOperation(type: GraphQLNamedType): Boolean {
+        return type.name.equals(OperationDefinition.Operation.QUERY.toString(), ignoreCase = true)
+            || type.name.equals(OperationDefinition.Operation.MUTATION.toString(), ignoreCase = true)
+            || type.name.equals(OperationDefinition.Operation.SUBSCRIPTION.toString(), ignoreCase = true)
     }
 }
