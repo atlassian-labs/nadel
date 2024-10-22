e6d75744d30014e47e72fd2b60868b74f7b41671
Felipe Reis <felipe.gdr@gmail.com>
2024-10-22 15:24:14 +1100

    Throw error from transform instead of returning error transform instructions

--- test/src/test/kotlin/graphql/nadel/tests/next/fixtures/partition/PartitionWithConflictingRoutingValuesTestSnapshot.kt
+++ test/src/test/kotlin/graphql/nadel/tests/next/fixtures/partition/PartitionWithConflictingRoutingValuesTestSnapshot.kt
@@ -21,45 +21,7 @@ private suspend fun main() {
 @Suppress("unused")
 public class PartitionWithConflictingRoutingValuesTestSnapshot : TestSnapshot() {
     override val calls: List<ExpectedServiceCall> = listOf(
-            ExpectedServiceCall(
-                service = "things_service",
-                query = """
-                | query getPartitionedThings {
-                |   things(filter: {thingsIds : [{primaryId : "thing-1-primary:partition-A", secondaryId : "thing-1-secondary"}, {primaryId : "thing-2-different-partition:partition-A", secondaryId : "thing-2-secondary-different-partition:partition-B"}, {primaryId : "thing-3-primary-no-partition", secondaryId : "thing-3-secondary:partition-A"}, {primaryId : "thing-4-primary:partition-B", secondaryId : "thing-4-secondary"}]}) {
-                |     id
-                |     name
-                |   }
-                | }
-                """.trimMargin(),
-                variables = " {}",
-                result = """
-                | {
-                |   "data": {
-                |     "things": [
-                |       {
-                |         "id": "thing-1-primary",
-                |         "name": "THING-1-PRIMARY"
-                |       },
-                |       {
-                |         "id": "thing-2-different-partition",
-                |         "name": "THING-2-DIFFERENT-PARTITION"
-                |       },
-                |       {
-                |         "id": "thing-3-primary-no-partition",
-                |         "name": "THING-3-PRIMARY-NO-PARTITION"
-                |       },
-                |       {
-                |         "id": "thing-4-primary",
-                |         "name": "THING-4-PRIMARY"
-                |       }
-                |     ]
-                |   }
-                | }
-                """.trimMargin(),
-                delayedResults = listOfJsonStrings(
-                ),
-            ),
-        )
+            )
 
     /**
      * ```json
