f7c095e318d15f39ba1094fdcfad1f697be80313
Franklin Wang <fwang@atlassian.com>
2024-11-15 12:17:30 +1100

    Branch with SchemaTraverser to show it's slow

--- lib/src/main/java/graphql/nadel/validation/util/NadelGetReachableTypes.kt
+++ lib/src/main/java/graphql/nadel/validation/util/NadelGetReachableTypes.kt
@@ -4,7 +4,7 @@ import graphql.language.UnionTypeDefinition
 import graphql.nadel.Service
 import graphql.nadel.definition.hydration.isHydrated
 import graphql.nadel.definition.virtualType.isVirtualType
-import graphql.nadel.engine.blueprint.NadelFastSchemaTraverser
+import graphql.nadel.engine.util.AnySDLNamedDefinition
 import graphql.nadel.engine.util.makeFieldCoordinates
 import graphql.nadel.engine.util.unwrapAll
 import graphql.nadel.validation.NadelValidationContext
@@ -15,7 +15,6 @@ import graphql.schema.GraphQLCompositeType
 import graphql.schema.GraphQLDirective
 import graphql.schema.GraphQLDirectiveContainer
 import graphql.schema.GraphQLEnumType
-import graphql.schema.GraphQLEnumValueDefinition
 import graphql.schema.GraphQLFieldDefinition
 import graphql.schema.GraphQLFieldsContainer
 import graphql.schema.GraphQLInputFieldsContainer
@@ -24,16 +23,22 @@ import graphql.schema.GraphQLInputObjectType
 import graphql.schema.GraphQLInterfaceType
 import graphql.schema.GraphQLList
 import graphql.schema.GraphQLModifiedType
-import graphql.schema.GraphQLNamedSchemaElement
 import graphql.schema.GraphQLNamedType
 import graphql.schema.GraphQLNonNull
 import graphql.schema.GraphQLNullableType
 import graphql.schema.GraphQLObjectType
 import graphql.schema.GraphQLOutputType
 import graphql.schema.GraphQLScalarType
+import graphql.schema.GraphQLSchemaElement
 import graphql.schema.GraphQLTypeReference
+import graphql.schema.GraphQLTypeVisitorStub
 import graphql.schema.GraphQLUnionType
 import graphql.schema.GraphQLUnmodifiedType
+import graphql.schema.SchemaTraverser
+import graphql.util.TraversalControl
+import graphql.util.TraversalControl.ABORT
+import graphql.util.TraversalControl.CONTINUE
+import graphql.util.TraverserContext
 
 context(NadelValidationContext)
 internal fun getReachableTypeNames(
@@ -47,210 +52,211 @@ internal fun getReachableTypeNames(
     fun add(name: String) = reachableTypeNames.add(name)
     fun addAll(elements: Collection<String>) = reachableTypeNames.addAll(elements)
 
-    val traverser = object : NadelFastSchemaTraverser.Visitor {
+    val serviceDefinitions = engineSchema.typeMap.values.filter { it.name in definitionNames }
+    val traverser = object : GraphQLTypeVisitorStub() {
         override fun visitGraphQLArgument(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLArgument,
-        ): Boolean {
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
             add(node.type.unwrapAll().name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLUnionType(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLUnionType,
-        ): Boolean {
-            visitTypeGuard(node) { return false }
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
+            visitTypeGuard(node) { return ABORT }
             add(node.name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLInterfaceType(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLInterfaceType,
-        ): Boolean {
-            visitTypeGuard(node) { return false }
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
+            visitTypeGuard(node) { return ABORT }
             add(node.name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLEnumType(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLEnumType,
-        ): Boolean {
-            visitTypeGuard(node) { return false }
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
+            visitTypeGuard(node) { return ABORT }
             add(node.name)
-            return true
-        }
-
-        override fun visitGraphQLEnumValueDefinition(
-            parent: GraphQLNamedSchemaElement?,
-            node: GraphQLEnumValueDefinition,
-        ): Boolean {
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLFieldDefinition(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLFieldDefinition,
-        ): Boolean {
-            val parentNode = parent as GraphQLFieldsContainer
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
+            val parentNode = context.parentNode as GraphQLFieldsContainer
 
             // Don't look at fields contributed by other services
             if (parentNode.name in combinedTypeNames) {
                 if (service.name != fieldContributor[makeFieldCoordinates(parentNode.name, node.name)]!!.name) {
-                    return false
+                    return ABORT
                 }
             }
 
-            return !node.isHydrated()
+            return if (node.isHydrated()) {
+                // Do not collect output type, hydrations do not require the type to be defined in the service
+                ABORT
+            } else {
+                // Will visit the output type
+                CONTINUE
+            }
         }
 
         override fun visitGraphQLInputObjectField(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLInputObjectField,
-        ): Boolean {
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
             add(node.type.unwrapAll().name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLInputObjectType(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLInputObjectType,
-        ): Boolean {
-            visitTypeGuard(node) { return false }
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
+            visitTypeGuard(node) { return ABORT }
             add(node.name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLList(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLList,
-        ): Boolean {
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
             add(node.unwrapAll().name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLNonNull(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLNonNull,
-        ): Boolean {
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
             add(node.unwrapAll().name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLObjectType(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLObjectType,
-        ): Boolean {
-            visitTypeGuard(node) { return false }
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
+            visitTypeGuard(node) { return ABORT }
 
             // Don't look at union members defined by external services
-            if (parent is GraphQLUnionType && isUnionMemberExempt(service, parent, node)) {
-                return false
+            val parentNode = context.parentNode
+            if (parentNode is GraphQLUnionType && isUnionMemberExempt(service, parentNode, node)) {
+                return ABORT
             }
 
             add(node.name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLScalarType(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLScalarType,
-        ): Boolean {
-            visitTypeGuard(node) { return false }
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
+            visitTypeGuard(node) { return ABORT }
             add(node.name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLTypeReference(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLTypeReference,
-        ): Boolean {
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
             add(node.name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLModifiedType(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLModifiedType,
-        ): Boolean {
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
             add(node.unwrapAll().name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLCompositeType(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLCompositeType,
-        ): Boolean {
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
             add(node.name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLDirective(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLDirective,
-        ): Boolean {
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
             // We don't care about directives, not a Nadel runtime concern
             // GraphQL Java will do validation on them for us
-            return false
+            return ABORT
         }
 
         override fun visitGraphQLDirectiveContainer(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLDirectiveContainer,
-        ): Boolean {
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
             add(node.name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLFieldsContainer(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLFieldsContainer,
-        ): Boolean {
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
             add(node.name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLInputFieldsContainer(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLInputFieldsContainer,
-        ): Boolean {
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
             add(node.name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLNullableType(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLNullableType,
-        ): Boolean {
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
             add(node.unwrapAll().name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLOutputType(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLOutputType,
-        ): Boolean {
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
             add(node.unwrapAll().name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLUnmodifiedType(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLUnmodifiedType,
-        ): Boolean {
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
             add(node.name)
-            return true
+            return CONTINUE
         }
 
         override fun visitGraphQLAppliedDirective(
-            parent: GraphQLNamedSchemaElement?,
             node: GraphQLAppliedDirective,
-        ): Boolean {
+            context: TraverserContext<GraphQLSchemaElement>,
+        ): TraversalControl {
             // Don't look into applied directives. Could be a shared directive.
             // As long as the schema compiled then we don't care.
-            return false
+            return ABORT
         }
 
         /**
@@ -268,11 +274,9 @@ internal fun getReachableTypeNames(
         }
     }
 
-    NadelFastSchemaTraverser().traverse(
-        schema = engineSchema,
-        roots = definitionNames,
-        visitor = traverser,
-    )
+    SchemaTraverser { element ->
+        element.children
+    }.depthFirst(traverser, serviceDefinitions)
 
     return reachableTypeNames
 }
