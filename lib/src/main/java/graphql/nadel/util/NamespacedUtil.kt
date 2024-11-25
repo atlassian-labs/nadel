0d155fdad089ecf54e80b26841229b169cf77712
Franklin Wang <9077461+gnawf@users.noreply.github.com>
2024-11-25 12:04:47 +1100

    Use precomputed namespace typenames (#632)
    * Use precomputed namespace typenames
    
    * Huh

--- lib/src/main/java/graphql/nadel/util/NamespacedUtil.kt
+++ lib/src/main/java/graphql/nadel/util/NamespacedUtil.kt
@@ -1,30 +1,22 @@
 package graphql.nadel.util
 
 import graphql.language.ObjectTypeDefinition
-import graphql.language.ObjectTypeExtensionDefinition
 import graphql.nadel.Service
-import graphql.nadel.engine.util.operationTypes
-import graphql.nadel.engine.util.unwrapAll
+import graphql.nadel.engine.util.isExtensionDef
 import graphql.nadel.schema.NadelDirectives.namespacedDirectiveDefinition
 import graphql.normalized.ExecutableNormalizedField
 import graphql.schema.GraphQLFieldDefinition
-import graphql.schema.GraphQLNamedOutputType
-import graphql.schema.GraphQLNamedType
-import graphql.schema.GraphQLObjectType
 import graphql.schema.GraphQLSchema
 
 object NamespacedUtil {
-    fun serviceOwnsNamespacedField(namespacedObjectType: GraphQLObjectType, service: Service): Boolean {
-        return serviceOwnsNamespacedField(namespacedObjectType.name, service)
-    }
-
     fun serviceOwnsNamespacedField(namespacedObjectTypeName: String, service: Service): Boolean {
         return service
             .definitionRegistry
-            .getDefinitionsOfType<ObjectTypeDefinition>()
-            .stream() // the type can't be an extension in the owning service
-            .filter { it !is ObjectTypeExtensionDefinition }
-            .anyMatch { it.name == namespacedObjectTypeName }
+            .getDefinitions(namespacedObjectTypeName)
+            .asSequence()
+            .filterIsInstance<ObjectTypeDefinition>()
+            .filterNot { it.isExtensionDef }
+            .any { it.name == namespacedObjectTypeName }
     }
 
     fun isNamespacedField(overallField: ExecutableNormalizedField, schema: GraphQLSchema): Boolean {
@@ -37,21 +29,5 @@ object NamespacedUtil {
 
     fun isNamespacedField(definition: GraphQLFieldDefinition): Boolean {
         return definition.hasAppliedDirective(namespacedDirectiveDefinition.name)
-    }
-
-    /**
-     * Even if this returns "true", nothing prevents this type from being used in a "non-namespace" field
-     */
-    fun isNamespaceType(type: GraphQLNamedType, schema: GraphQLSchema): Boolean {
-        val namespaceFieldsWithThisType = schema.operationTypes
-            .asSequence()
-            .flatMap { it.fieldDefinitions }
-            .filter { isNamespacedField(it) }
-            .map { it.type.unwrapAll() }
-            .filterIsInstance<GraphQLNamedOutputType>()
-            .filter { it.name == type.name }
-            .toList()
-
-        return namespaceFieldsWithThisType.size == 1
     }
 }
