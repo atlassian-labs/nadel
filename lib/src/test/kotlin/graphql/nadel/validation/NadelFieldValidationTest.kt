3680707baf7770a151324ea6f238064fe7c1de7e
kpriyam <kpriyam@atlassian.com>
2024-12-03 22:04:14 +0530

    added tests for validation

--- lib/src/test/kotlin/graphql/nadel/validation/NadelFieldValidationTest.kt
+++ lib/src/test/kotlin/graphql/nadel/validation/NadelFieldValidationTest.kt
@@ -679,11 +679,6 @@ class NadelFieldValidationTest : DescribeSpec({
             assert(error.message == "The overall field Query.fieldA defines argument id which does not exist in service test field Query.fieldA")
         }
 
-        // it("fails if schema contains all fields as hidden") {
-        //     //Write a test to check if all fields are hidden. Take reference from multiple test files that uses
-        //     //hidden directive. Need to test code : graphql.nadel.validation.NadelFieldValidation.validate(graphql.nadel.validation.NadelServiceSchemaElement.FieldsContainer, java.util.List<? extends graphql.schema.GraphQLFieldDefinition>)
-        // }
-
         it("fails if schema contains all fields as hidden") {
             val fixture = NadelValidationTestFixture(
                 overallSchema = mapOf(
@@ -708,27 +703,5 @@ class NadelFieldValidationTest : DescribeSpec({
             val error = errors.assertSingleOfType<NadelSchemaValidationError>()
             assert(error.message == "No fields found without @hidden directive on the underlying type Query on service test")
         }
-
-        // it("fails if schema contains all fields as hidden") {
-        //     val fixture = NadelValidationTestFixture(
-        //         overallSchema = mapOf(
-        //             "test" to """
-        //                 type Query {
-        //                     echo: String
-        //                 }
-        //             """.trimIndent(),
-        //         ),
-        //         underlyingSchema = mapOf(
-        //             "test" to """
-        //                 type Query {
-        //                     echo: String
-        //                 }
-        //             """.trimIndent(),
-        //         ),
-        //     )
-        //
-        //     val errors = validate(fixture)
-        //     assert(errors.map { it.message }.isEmpty())
-        // }
     }
 })
